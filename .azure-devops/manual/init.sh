1fe1cbbd6a54962996ebf82d8bae01bfe2021f99
Harshwardhan Deshmukh <58629753+Harshwardhan-Deshmukh@users.noreply.github.com>
2025-02-21 21:39:38 +0530

    Update init.sh

--- .azure-devops/manual/init.sh
+++ .azure-devops/manual/init.sh
@@ -2,7 +2,6 @@
 RESOURCE_GROUP_NAME="azure-devops-user-rg"
 AZURE_DEVOPS_USER="azure-devops-user-mi"
 TERRAFORM_BACKEND_STORAGE_ACCOUNT="satfstateecpeus01" # change this name as storage account should be globally unique
-CONTAINER_NAME="exchangepay-tfstate"
 
 # Create a Resource Group
 az group create --name $RESOURCE_GROUP_NAME --location eastus
@@ -21,11 +20,3 @@ az role assignment create --assignee $CLIENT_ID --role "Contributor" --scope /su
 
 # Create a Storage Account and Blob Container inside it for Terraform to Store its State file
 az storage account create --name $TERRAFORM_BACKEND_STORAGE_ACCOUNT --resource-group $RESOURCE_GROUP_NAME --sku "Standard_LRS" --encryption-services "blob" --public-network-access "Enabled" 1> /dev/null
-az storage account blob-service-properties update --resource-group $RESOURCE_GROUP_NAME --account-name $TERRAFORM_BACKEND_STORAGE_ACCOUNT --enable-versioning "true" --enable-delete-retention "true" --delete-retention-days "7" --enable-container-delete-retention "true" --container-delete-retention-days "7" 1> /dev/null
-az storage container create --name $CONTAINER_NAME --account-name $TERRAFORM_BACKEND_STORAGE_ACCOUNT --resource-group $RESOURCE_GROUP_NAME --auth-mode login
-
-# Fetch the Storage Account ID for Role Assignment
-STORAGE_ACCOUNT_ID=$(az storage account show --name $TERRAFORM_BACKEND_STORAGE_ACCOUNT --resource-group $RESOURCE_GROUP_NAME --query id --output tsv)
-
-# Assign Storage Blob Data Contributor role to the Managed Identity allowing it to makeblob  related changes in the Storage Container
-az role assignment create --assignee $CLIENT_ID --role "Storage Blob Data Contributor" --scope $STORAGE_ACCOUNT_ID
